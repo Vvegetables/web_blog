d9f86a6af20ec2c4a34b1024959497ac4726d1d6
Zcxu <625736074@qq.com>
2018-11-24 23:49:59 +0800

    seven

--- front-app/src/containers/LoginBottom.js
+++ front-app/src/containers/LoginBottom.js
@@ -5,14 +5,21 @@ export default class LoginBottom extends Component{
     
     render(){
         return (
-            <div className="login-bottom">
-                <ul>
-                    <li>icon1</li>
-                    <li>icon1</li>
-                    <li>icon1</li>
-                    <li>icon1</li>
-                </ul>
+            <div className="kk">
+                <div className="more-sign">
+                    <h6 className="login-line">其他登录方式</h6>
+                
+                    <div className="login-bottom">
+                        <ul>
+                            <li>icon1</li>
+                            <li>icon1</li>
+                            <li>icon1</li>
+                            <li>icon1</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+            
         )
     }
 }
